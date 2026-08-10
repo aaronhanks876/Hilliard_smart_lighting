612361b8186ceef589093b69a7ff1c2abee776ac
von9880 <tony.auron.brown@gmail.com>
2026-08-10 12:39:44 -0400

    Add files via upload

---
+++ music_procesing/config.py
@@ -0,0 +1,49 @@
+
+
+# config.py
+
+# Audio capture
+
+SPEAKER_NAME = None  # Use Windows default
+
+WINDOW_SIZE = 1024
+
+# MQTT
+
+MQTT_BROKER = "192.168.60.6"
+MQTT_PORT = 1883
+MQTT_USERNAME = "MusicBox"
+MQTT_PASSWORD = "HilliardMusicBox"
+
+# Processing
+
+SMOOTHING = 0.7
+BEAT_SENSITIVITY = 2.2
+# config.py
+
+# Audio capture
+
+LOOPBACK_DEVICE = 2   # Change this to your loopback device index
+WINDOW_SIZE = 1024
+
+# MQTT
+
+MQTT_BROKER = "192.168.60.6"
+MQTT_PORT = 1883
+MQTT_USERNAME = "MusicBox"
+MQTT_PASSWORD = "HilliardMusicBox"
+
+
+
+
+# =========================
+# FFT Frequency Bands
+# =========================
+
+BASS_RANGE = (40, 180)
+
+LOW_MID_RANGE = (180, 600)
+
+MID_RANGE = (600, 3000)
+
+TREBLE_RANGE = (3000, 10000)
